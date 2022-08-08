8e5c926548cc4297efe73d3dd3de0a7b40f047c8
ViniciusCortat <31554254+ViniciusCortat@users.noreply.github.com>
2022-08-08 10:13:57 -0300

    teste-pratico

---
+++ SistemaCompras-v3/SistemaCompra.Application/SolicitacaoCompra/Command/RegistrarCompra/RegistrarCompraCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+
+namespace SistemaCompra.Application.SolicitacaoCompra.Command.RegistrarCompra
+{
+    public class RegistrarCompraCommand : IRequest<bool>
+    {
+        public string UsuarioSolicitante { get; set; }
+        public string NomeFornecedor { get; set; }
+        public DateTime Data { get; set; }
+        public decimal TotalGeral { get; set; }
+        public int Situacao { get; set; }
+        public int CondicaoPagamento { get; set; }
+    }
+}
